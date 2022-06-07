8d883cca8c71a77368fcdb4a39cab58f636dcf49
AnastasiaZabor <anastasiazaborskaa@gmail.com>
2022-06-07 12:21:27 +0700

    Project created

---
+++ CSharpHomework1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpHomework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
