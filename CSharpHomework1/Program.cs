18af72e964d185e222d506a061938f187065e5aa
AnastasiaZabor <anastasiazaborskaa@gmail.com>
2022-06-07 12:32:36 +0700

    Task1

--- CSharpHomework1/Program.cs
+++ CSharpHomework1/Program.cs
@@ -6,7 +6,26 @@ namespace CSharpHomework1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Task1(); 
+        }
+
+        public static void Task1()
+        {
+            Console.WriteLine("VVedite num1");
+            int num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("VVedite num2");
+            int num2 = Int32.Parse(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1);
+            }
+            else
+            {
+                Console.WriteLine(num2);
+            }
+            
+            Console.ReadLine();
         }
     }
 }
