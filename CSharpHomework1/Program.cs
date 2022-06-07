b6e39926e031cb86f24e3109229cfc443c63e1ce
AnastasiaZabor <anastasiazaborskaa@gmail.com>
2022-06-07 16:34:05 +0700

    Tasks 2-4

--- CSharpHomework1/Program.cs
+++ CSharpHomework1/Program.cs
@@ -6,15 +6,18 @@ namespace CSharpHomework1
     {
         static void Main(string[] args)
         {
-           Task1(); 
+           //Task1(); 
+           //Task2();
+           //Task3();
+           //Task4();
         }
 
         public static void Task1()
         {
             Console.WriteLine("VVedite num1");
-            int num1 = Int32.Parse(Console.ReadLine());
+            int num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("VVedite num2");
-            int num2 = Int32.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
 
             if (num1 > num2)
             {
@@ -25,6 +28,67 @@ namespace CSharpHomework1
                 Console.WriteLine(num2);
             }
             
+            Console.ReadLine();
+        }
+        
+        
+        public static void Task2()
+        {
+            Console.WriteLine("VVedite num1");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("VVedite num2");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("VVedite num3");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine(num1);
+            }
+            else
+            {
+                if (num2 > num3)
+                {
+                    Console.WriteLine(num2);
+                }
+                else
+                {
+                    Console.WriteLine(num3);
+                }
+            }
+            
+            Console.ReadLine();
+        }
+        
+        
+        public static void Task3()
+        {
+            Console.WriteLine("VVedite num");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+            
+            Console.ReadLine();
+        }
+
+
+        public static void Task4()
+        {
+            Console.WriteLine("VVedite N");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i < n; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
             Console.ReadLine();
         }
     }
