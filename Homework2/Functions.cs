b74cf6e2297a8d1fbc273d45a4cc21ff2bf65767
AnastasiaZabor <anastasiazaborskaa@gmail.com>
2022-06-15 20:10:59 +0700

    Homework2

---
+++ Homework2/Functions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Homework2
+{
+    public static class Functions
+    {
+        public static void Task1()
+        {
+            Console.WriteLine("Enter a three-digit number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(TakeSecondNumber(num));
+        }
+
+
+        public static void Task2()
+        {
+            Console.WriteLine("Enter day of the week");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(DayOfTheWeek(num));
+        }
+        
+        
+        
+        private static int TakeSecondNumber(int num)
+        {
+            return num / 10 % 10;
+        }
+
+
+        private static bool DayOfTheWeek(int num)
+        {
+            switch (num)
+            {
+                case 1: return false;
+                case 2: return false;
+                case 3: return false;
+                case 4: return false;
+                case 5: return false;
+                case 6: return true;
+                case 7: return true;
+                default: return false;
+            }
+        }
+    }
+}
