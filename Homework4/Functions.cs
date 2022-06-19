b2fed40640f1dc608b2020ec0ade16d0ef285894
AnastasiaZabor <anastasiazaborskaa@gmail.com>
2022-06-19 11:47:35 +0700

    Homework4

---
+++ Homework4/Functions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace Homework4
+{
+    public class Functions
+    {
+        public static void Task1()
+        {
+            Console.WriteLine("Enter num1 and num2");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int result = Exponentiation(num1, num2);
+            Console.WriteLine(result);
+        }
+
+        public static void Task2()
+        {
+            Console.WriteLine("Enter num");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(SumOfNumbers(num));
+        }
+
+        public static void Task3()
+        {
+            int[] arr = new int[8];
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine($"Enter num{i+1}");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(Print(arr));
+        }
+        
+        private static int Exponentiation(int num1, int num2)
+        {
+            return (int) Math.Pow(num1, num2);
+        }
+
+        private static int SumOfNumbers(int num)
+        {
+            int diff = num;
+            int sum = 0;
+            
+            while (diff / 10 != 0)
+            {
+                sum += (diff % 10);
+                diff /= 10;
+            }
+
+            sum += diff;
+            return sum;
+        }
+        
+        private static string Print(int[] arr)
+        {
+            return string.Join(", ", arr);
+        }
+            
+    }
+}
---
+++ Homework4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Functions.Task1();
+            //Functions.Task2();
+            Functions.Task3();
+        }
+    }
+}
